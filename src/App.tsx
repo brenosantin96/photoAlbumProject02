78774abd92cb5aec47caab5afc7b9578275c1bd3
Breno Santin <brenolimaa96@gmail.com>
2022-10-26 16:03:01 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as C from './App.styles';
+import React from 'react'
+
+export function App() {
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header> Galeria de fotos </C.Header>
+
+        {/* área upload */ }
+
+        {/* lista de fotos  */ }
+        
+      </C.Area>
+
+    </C.Container>
+  )
+}
+
